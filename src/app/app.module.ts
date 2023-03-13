2123b6fc2f31c775bfa083ac746e01d9dde0b1ba
Yaqi <yaqiye22@gmail.com_>
2023-03-13 18:24:56 +0100

    Guardado 13_03_2023 wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador.component';
+
+
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent{
+  heroes: string[] = ['Spiderman','Ironman','Hulk','Thor','Capitán América'];
+  heroeBorrado: string = '';
+
+  borrarHeroe(){
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+}
